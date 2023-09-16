d33437cce7ffc0c4009beb3c34d90574d430acb1
Zendard <thijsmuylaert@hotmail.com>
2023-09-16 20:37:09 +0200

    Create initial structure

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import Express from "express";
+const port = parseInt(Bun.env.PORT || "");
+
+const app = Express();
+app.set("view engine", "ejs");
+app.use(Express.static("./public"));
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}...`);
+});
