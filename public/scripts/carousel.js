d381d3753c73412f6a1fbf20ac6eafaf54dbdd7b
Zendard <thijsmuylaert@hotmail.com>
2023-09-16 22:58:21 +0200

    Make index.ejs and initial styling

---
+++ public/scripts/carousel.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+const slides = document.getElementsByClassName("img-slide");
+showSlides();
+
+function showSlides() {
+	console.log(slideIndex);
+	for (i = 0; i < 3; i++) {
+		if (i != slideIndex) {
+			slides[i].classList.remove("fade-in");
+			slides[i].classList.add("fade-out");
+			setTimeout(displayNone.bind(null, i), 2000);
+		} else {
+			slides[i].style.display = "block";
+			slides[i].classList.remove("fade-out");
+			slides[i].classList.add("fade-in");
+		}
+	}
+	slideIndex++;
+	if (slideIndex > 2) {
+		slideIndex = 0;
+	}
+	setTimeout(showSlides, 5000);
+}
+
+function displayNone(slide) {
+	slides[slide].style.display = "none";
+}
