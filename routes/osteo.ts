b728bd458348fde6bbee189158344513095defb9
Zendard <thijsmuylaert@hotmail.com>
2023-09-17 14:57:32 +0200

    Add remaining osteo pages

--- routes/osteo.ts
+++ routes/osteo.ts
@@ -11,4 +11,12 @@ router.get("/filosofie", (req, res) => {
 	res.render("osteo-filosofie");
 });
 
+router.get("/werkwijze", (req, res) => {
+	res.render("osteo-werkwijze");
+});
+
+router.get("/indicaties", (req, res) => {
+	res.render("osteo-indicaties");
+});
+
 export default router;
