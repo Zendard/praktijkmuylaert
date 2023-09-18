4291bec788d3f5abdec3e00fc463844d66d3e327
Zendard <thijsmuylaert@hotmail.com>
2023-09-18 18:14:26 +0200

    Fix formatting

--- routes/kine.ts
+++ routes/kine.ts
@@ -1,22 +1,22 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
 
-router.use(express.static("./public"));
+router.use(express.static('./public'));
 
-router.get("/", (req, res) => {
-	res.render("kinesitherapie");
+router.get('/', (req, res) => {
+	res.render('kinesitherapie');
 });
 
-router.get("/werkwijze", (req, res) => {
-	res.render("kine-werkwijze");
+router.get('/werkwijze', (req, res) => {
+	res.render('kine-werkwijze');
 });
 
-router.get("/indicaties", (req, res) => {
-	res.render("kine-indicaties");
+router.get('/indicaties', (req, res) => {
+	res.render('kine-indicaties');
 });
 
-router.get("/proef2move", (req, res) => {
-	res.render("kine-proef2move");
+router.get('/proef2move', (req, res) => {
+	res.render('kine-proef2move');
 });
 
 export default router;
