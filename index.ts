011a903e34aa14ae1204a36b53cd6aa680d92866
Zendard <thijsmuylaert@hotmail.com>
2023-09-19 22:09:54 +0200

    Revert "Fix formatting"
    This reverts commit 4291bec788d3f5abdec3e00fc463844d66d3e327.

--- index.ts
+++ index.ts
@@ -1,35 +1,35 @@
-import express from 'express';
-import kine from './routes/kine';
-import osteo from './routes/osteo';
-import compression from 'compression';
-const port = parseInt(Bun.env.PORT ?? '');
+import express from "express";
+import kine from "./routes/kine";
+import osteo from "./routes/osteo";
+import compression from "compression";
+const port = parseInt(Bun.env.PORT || "");
 
 const app = express();
-app.set('view engine', 'ejs');
-app.use(express.static('./public'));
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
+app.set("views", "./views");
 app.use(compression());
 
-app.get('/', (req, res) => {
-	res.render('index');
+app.get("/", (req, res) => {
+	res.render("index");
 });
 
-app.get('/contact', (req, res) => {
-	res.render('contact');
+app.get("/contact", (req, res) => {
+	res.render("contact");
 });
 
-app.use('/kinesitherapie', kine);
-app.use('/osteopathie', osteo);
+app.use("/kinesitherapie", kine);
+app.use("/osteopathie", osteo);
 
 app.get(
-	'/*',
+	"/*",
 	(req, res, next) => {
-		res.render('404');
+		res.render("404");
 		next();
 	},
 	(req, res) => {
 		res.sendStatus(404);
-	},
+	}
 );
 
 app.listen(port, () => {
--- routes/osteo.ts
+++ routes/osteo.ts
@@ -1,22 +1,22 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-router.use(express.static('./public'));
+router.use(express.static("./public"));
 
-router.get('/', (req, res) => {
-	res.render('osteopathie');
+router.get("/", (req, res) => {
+	res.render("osteopathie");
 });
 
-router.get('/filosofie', (req, res) => {
-	res.render('osteo-filosofie');
+router.get("/filosofie", (req, res) => {
+	res.render("osteo-filosofie");
 });
 
-router.get('/werkwijze', (req, res) => {
-	res.render('osteo-werkwijze');
+router.get("/werkwijze", (req, res) => {
+	res.render("osteo-werkwijze");
 });
 
-router.get('/indicaties', (req, res) => {
-	res.render('osteo-indicaties');
+router.get("/indicaties", (req, res) => {
+	res.render("osteo-indicaties");
 });
 
 export default router;
