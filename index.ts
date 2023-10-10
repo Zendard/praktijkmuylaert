1734a51ee62ef3d5bbdc03364d5c643e9cb7b785
Zendard <thijsmuylaert@hotmail.com>
2023-10-10 18:08:13 +0200

    Add favicon

--- index.ts
+++ index.ts
@@ -2,10 +2,13 @@ import express from "express";
 import kine from "./routes/kine";
 import osteo from "./routes/osteo";
 import compression from "compression";
+import favicon from "serve-favicon";
 const port = parseInt(Bun.env.PORT || "");
 
 const app = express();
 app.set("view engine", "ejs");
+console.log(__dirname+"/public/favicon.ico");
+app.use(favicon(__dirname+"/public/favicon.ico"));
 app.use(express.static("./public"));
 app.set("views", "./views");
 app.use(compression());
