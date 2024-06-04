489c9752b203c7b8e3528c614c92f05814494c75
Zendard <thijsmuylaert@hotmail.com>
2024-06-04 08:58:04 +0200

    Add scroll animations

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,14 @@ async fn index() -> Option<NamedFile> {
     NamedFile::open("views/index.html").await.ok()
 }
 
+#[get("/contact")]
+async fn contact() -> Option<NamedFile> {
+    NamedFile::open("views/contact.html").await.ok()
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, contact])
         .mount("/", rocket::fs::FileServer::from("public"))
 }
