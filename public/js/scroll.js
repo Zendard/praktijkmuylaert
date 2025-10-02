8b5052f9e84e39ec943277adaea95e7e4b75ad33
Zendard <thijsmuylaert@hotmail.com>
2025-10-02 19:10:03 +0200

    Fix scroll reveal happening to late on mobile

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -3,7 +3,7 @@ function reveal() {
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+    var elementVisible = windowHeight / 1080 * 150;
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     } else {
